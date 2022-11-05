de479688868134da6ba5c99a1cc14a32ec72591d
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-05 11:03:50 +0900

    show data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/card/Card';
 import {getAllPokemon, getPokemon} from './utils/pokemon.js';
 
 function App() {
@@ -37,7 +38,11 @@ function App() {
         <h1>ロード中・・・</h1>
       ) : (
         <>
-          <h1>データを取得しました</h1>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />
+            })}
+          </div>
         </>
       )}
     </div>
